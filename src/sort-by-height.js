8785650e0429df1ccfbb60a168b96ea7e98b4d47
Ceneve <boriskozlow@gmail.com>
2025-01-05 18:12:36 +0300

    feat: add solutions to sort by height, sum digits, what-season

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,9 +11,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  const length = arr.length;
+  let indexArray = [];
+  let resultArray = [];
+  for (let i = 0; i < length; i += 1) {
+      if (arr[i] === -1) {
+          indexArray.push(i)
+      }
+  }
+  const sortedArray = arr.filter(value => value !== -1).sort((a, b) => a - b);
+  let sortedArrayIndex = 0;
+  for (let i = 0; i < length; i += 1) {
+      if(indexArray.includes(i)) {
+          resultArray.push(-1);
+      }
+      else {
+          resultArray.push(sortedArray[sortedArrayIndex]);
+          sortedArrayIndex += 1;
+      }
+  }
+  return resultArray;
 }
 
 module.exports = {
